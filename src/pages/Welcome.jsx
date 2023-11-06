8877d8109889a2add60b1858f5a24f113de9164f
Nilshan Deemantha <ndeemantha1999@gmail.com>
2023-11-06 16:51:50 +0530

    Add user input field

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./welcome.css";
+import TextField from "@mui/material/TextField";
 
 export function Welcome() {
   const location = useLocation();
   const userName = location.state;
-  console.log(userName);
+
+  const [userInput, setUserInput] = useState("");
+
   return (
     <div className="welcome-container">
-      <h1>Welcome</h1>
-      <h2>{userName}</h2>
+      <h1>Welcome {userName}!</h1>
+      <div className="user-input">
+        {userInput.trim() && (
+          <>
+            <h2>User Input</h2>
+            <p>{userInput}</p>
+          </>
+        )}
+        <TextField
+          id="outlined-error-helper-text"
+          label="User Input"
+          size="small"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
