f219d0ba0319c690d2de9189e1ae7a542ea8f869
Nilshan Deemantha <ndeemantha1999@gmail.com>
2023-11-07 16:32:53 +0530

    Cahnge the routing and code structure into ES6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import Login from "./pages/Login";
-import { Welcome } from "./pages/Welcome";
+import Welcome from "./pages/Welcome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css"
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/welcome" element={<Welcome />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
