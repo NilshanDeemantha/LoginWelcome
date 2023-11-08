aa6a1b62f60c9a102c1832a09580bf94830c00dc
Nilshan Deemantha <ndeemantha1999@gmail.com>
2023-11-08 11:44:58 +0530

    Connect backend server to authenticate

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -5,9 +5,6 @@ import "./login.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-const testUserName = "testUser";
-const testPassword = "abc123";
-
 const Login = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
@@ -15,30 +12,48 @@ const Login = () => {
   const [passwordErr, setPasswordErr] = useState("");
   const [userNameErr, setUserNameErr] = useState("");
 
-  function handleLogin() {
-    let hasErrors = false;
+  const handleLogin = async () => {
     setUserNameErr("");
     setPasswordErr("");
-    if (!userName.trim()) {
-      hasErrors = true;
+    if (!userName.trim() && !password.trim()) {
       setUserNameErr("Please Enter User Name");
-    } else if (userName.trim() !== testUserName) {
-      hasErrors = true;
-      setUserNameErr("Invalid User Name");
-    }
-    if (!password.trim()) {
-      hasErrors = true;
       setPasswordErr("Please Enter Passowrd");
-    } else if (password.trim() !== testPassword) {
-      hasErrors = true;
-      setPasswordErr("Invalid Password");
+      return;
     }
-    if (!hasErrors) {
-      setPassword("");
-      setUserName("");
-      navigate("/welcome", { state: userName });
-    }
-  }
+
+    try {
+      const response = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userName, password }),
+      });
+      if (response.status === 200) {
+        setPassword("");
+        setUserName("");
+        navigate("/welcome", { state: userName });
+      } else if (response.status === 401) {
+        const result = await response.json();
+        if (result.msg === "Both Are invalid") {
+          setUserNameErr(
+            !userName.trim() ? "Please Enter User Name" : "Invalid User Name"
+          );
+          setPasswordErr(
+            !password.trim() ? "Please Enter Passowrd" : "Invalid Password"
+          );
+        } else if (result.msg === "Username invalid") {
+          setUserNameErr(
+            !userName.trim() ? "Please Enter User Name" : "Invalid User Name"
+          );
+        } else if (result.msg === "Password invalid") {
+          setPasswordErr(
+            !password.trim() ? "Please Enter Passowrd" : "Invalid Password"
+          );
+        }
+      }
+    } catch (err) {}
+  };
 
   return (
     <div className="container">
