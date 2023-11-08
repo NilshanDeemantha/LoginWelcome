1cd8503d1835b43713acd402df726708fc047e1b
Nilshan Deemantha <ndeemantha1999@gmail.com>
2023-11-08 16:33:43 +0530

    Navigate http://localhost:3000 into login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import Login from "./pages/Login";
 import Welcome from "./pages/Welcome";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/welcome" element={<Welcome />} />
         </Routes>
